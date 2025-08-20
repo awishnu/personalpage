91d39e9c67b244bf8164b0f6f95461420e0ca4e7
Ahmad Wishnu <ahmadwishnu@gmail.com>
2025-08-20 09:41:28 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll effect for navigation links
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
